8569f2d81dc3520bf987b9a841b183a87f71b2ce
Jodi Diehl <29265610+jdiehl99@users.noreply.github.com>
2017-09-09 09:55:30 -0400

    placed results in unordered list and added comments for profile image to be added

--- assets/js/actors.js
+++ assets/js/actors.js
@@ -1,27 +1,32 @@
 $(document).on("click", ".actors", function () {
     event.preventDefault();
 
-    // get country from button attribute
-    var countryShow = $(this).attr("data-country");
     // get first name from button attribute
     // var searchTerm = $(this).attr("data-name");
-    searchTerm = 'julia';
+searchTerm = "steven";
+    console.log("searchTerm",searchTerm);
     // hide the buttonsDiv
     $("#buttonsDiv").hide();
     // create the queryURL to request the data
     var queryURL = 'https://api.themoviedb.org/3/search/person?api_key=0b67c20dc0c60917a8d87eb35c0cd032&language=en-US&query=' + searchTerm + '&page=1&include_adult=false';
 
-
+// send the request through ajax
     $.ajax({
         url: queryURL,
         method: 'GET'
-    }).done(function (results) {
-        console.log(results);
-
-        // var data = results.data;
-        // for (var i = 0; i < data.length; i++) {
-
-        // }
+    }).done(function (response) {
+        console.log(response);
+        // go through results and display list of name
+        var data = response.results;
+        ul = $("<ul>"); 
+        for (var i = 0; i < data.length; i++) {
+            ul.append('<li>'+data[i].name+'</li>');
+        }
+        $("#dataShow").append(ul);    // add the list to the dataShow div
     });
 
 });
+
+// URL for photo of actor https://image.tmdb.org/t/p/w92{profile_path}
+// larger photo available at w185 and smaller at w45
+// link to search results if we want to include that https://www.themoviedb.org/search?query=julia+roberts
