4f1bb99efde6037ed75144bc282240c00d1842a1
Jodi Diehl <29265610+jdiehl99@users.noreply.github.com>
2017-09-09 09:24:50 -0400

    ajax successfully requesting list of actors by name search through themoviedb API

--- assets/js/actors.js
+++ assets/js/actors.js
@@ -1,7 +1,27 @@
 $(document).on("click", ".actors", function () {
-    console.log("actors button was clicked");
     event.preventDefault();
 
+    // get country from button attribute
     var countryShow = $(this).attr("data-country");
-    $("#dataShow").append('<p>Actors page - ' + countryShow + ' was passed through from button on Name Page</p>');
+    // get first name from button attribute
+    // var searchTerm = $(this).attr("data-name");
+    searchTerm = 'julia';
+    // hide the buttonsDiv
+    $("#buttonsDiv").hide();
+    // create the queryURL to request the data
+    var queryURL = 'https://api.themoviedb.org/3/search/person?api_key=0b67c20dc0c60917a8d87eb35c0cd032&language=en-US&query=' + searchTerm + '&page=1&include_adult=false';
+
+
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).done(function (results) {
+        console.log(results);
+
+        // var data = results.data;
+        // for (var i = 0; i < data.length; i++) {
+
+        // }
+    });
+
 });
