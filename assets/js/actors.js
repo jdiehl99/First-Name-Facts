f64e2fb51666f1a117b3f241cb061b56e2d47128
Jodi Diehl <29265610+jdiehl99@users.noreply.github.com>
2017-09-09 13:14:53 -0400

    added photo of actor and placeholder when no image is available

--- assets/js/actors.js
+++ assets/js/actors.js
@@ -4,29 +4,34 @@ $(document).on("click", ".actors", function () {
     // get first name from button attribute
     var searchTerm = $(this).attr("data-name");
 
-    console.log("searchTerm",searchTerm);
     // hide the buttonsDiv
     $("#buttonsDiv").hide();
     // create the queryURL to request the data
     var queryURL = 'https://api.themoviedb.org/3/search/person?api_key=0b67c20dc0c60917a8d87eb35c0cd032&language=en-US&query=' + searchTerm + '&page=1&include_adult=false';
 
-// send the request through ajax
+    // send the request through ajax
     $.ajax({
         url: queryURL,
         method: 'GET'
     }).done(function (response) {
-        console.log(response);
         // go through results and display list of names
         var data = response.results;
-        udiv = $('<div class="pure-g">'); 
+        udiv = $('<div class="actorList">');
         for (var i = 0; i < data.length; i++) {
-            udiv.append('<div class="pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">'+data[i].name+'</div>');
+            if (data[i].profile_path == null) {
+                // if there is no photo, show the placeholder image
+                var imgShow = "assets/images/noimage.png";
+            } else {
+                // show image from TMDB
+                var imgShow = 'https://image.tmdb.org/t/p/w92/' + data[i].profile_path;
+            }
+            udiv.append('<div><h3>'+data[i].name+'</h3><img src="' + imgShow + '"></div>');
         }
         $("#dataShow").append(udiv);    // add the list to the dataShow div
     });
 
 });
-
+// need to display in columns once CSS framework has been decided
 // URL for photo of actor https://image.tmdb.org/t/p/w92/{profile_path}
 // larger photo available at w185 and smaller at w45
 // link to search results if we want to include that https://www.themoviedb.org/search?query=julia+roberts
