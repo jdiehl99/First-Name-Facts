dd24ab1f0e362dc814e2daed7b60b72e7cef4bb2
mariogmazza <mariomazza01@gmail.com>
2017-09-10 11:45:18 -0400

    working history.js new

--- assets/js/history.js
+++ assets/js/history.js
@@ -1,7 +1,30 @@
-$(document).on("click", ".history", function () {
+
+$(document).ready(function(){
+    
+
+
+   $(document).on("click", ".history", function () {
+
     console.log("history button was clicked");
     event.preventDefault();
 
     var countryShow = $(this).attr("data-country");
-    $("#dataShow").append('<p>History page - ' + countryShow + ' was passed through from button on Name Page</p>');
+
+
+    var countryHistory;
+    if(countryShow!=="Unknown"){
+    countryHistory=factbook.countries[countryShow.toLowerCase()].data.introduction.background
+    }else{
+      countryHistory="Unknown Origin";
+      }
+
+    $("#dataShow").append('<p>'+countryHistory+'</p>');
+
+
+
 });
+
+
+    
+  });//end of $(document)
+
