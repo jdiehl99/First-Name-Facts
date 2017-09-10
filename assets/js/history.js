c84c17e20a37f6ab42b69c5655ada50f60511a2a
mariogmazza <mariomazza01@gmail.com>
2017-09-10 00:30:24 -0400

    working origin country info dysplay no css yet

--- assets/js/history.js
+++ assets/js/history.js
@@ -6,50 +6,18 @@ $(document).ready(function(){
     event.preventDefault();
 
     var countryShow = $(this).attr("data-country");
-    $("#dataShow").append('<p>History page - ' + countryShow + ' was passed through from button on Name Page</p>');
+
+
+    var x=countryShow.toLowerCase();
+    var first=factbook.countries[x].data.introduction.background
+
+
+    $("#dataShow").append('<p>'+first+'</p>');
+
+
+
 });
 
 
-
-
- 
-console.log(factbook.countries.argentina.data.introduction.background);
-
-
-
-
-  //   $("#addSearch").on("click",function(){
-  //     event.preventDefault();
-  //     var searchBox=$("#searchInput").val();
-  //     var urlApi="https://en.wikipedia.org/w/api.php?action=opensearch&search="+searchBox+"&format=json&callback=?";
-  //    console.log(urlApi[0]);
-      
-  //     $.ajax({
-  //   type: 'GET',   
-  //   dataType: "json",
-  //   url: urlApi,
-  //   async: false,
-  //   success: function(data){
-  //    $("#dataShow").html(" ");
-  //     for(var i=0;i<data[1].length;i++){
-  //     var headingTerm=data[1][i];
-  //     var termSumery=data[2][i];
-  //     var termLink=data[3][i];
-        
-  //       $("#dataShow").prepend("<a href="+termLink+"><ul id='output'><b>"+headingTerm+"</b><br><i>*"+termSumery+"*</i></ul></a>");
-        
-  //     console.log(headingTerm);
-  //     console.log(termSumery);
-  //     console.log(termLink);
-  //     }//for loop end
-  //   },
-  //       error:function(errorAlert){
-  //         alert("error")
-  //       }
-        
-  // });//ajax end
-      
-  //   });//end click botton
-    
     
   });//end of $(document)
--- assets/js/name.js
+++ assets/js/name.js
@@ -1,1 +1,95 @@
 
+var countryOrigin = "";
+var countryShow = "";
+var searchTerm = "";
+jQuery(($) => { // document on ready
+
+    $(document).on("click", "#doSearch", function () {
+        console.log("search button was clicked");
+        event.preventDefault();
+
+        // empty out the div where the results are displayed
+        $("#dataShow").empty();
+
+        // grab the name from the input box
+        var searchTerm = $("#searchInput").val().trim();
+
+        // send the request to BehindTheName using ajax
+        $.ajax({
+            type: "GET",
+            url: "https://www.behindthename.com/api/lookup.php?name=" + searchTerm + "&key=jo493898",
+            dataType: "xml",
+            success: function (xml) {
+
+                // grab the results from the XML data returned from BehindTheName
+                var originInfo = $(xml).find('usage').text();
+                // console.log("origin info", originInfo);
+                // if no origin was returned, mark as unknown
+                if (originInfo == "") {
+                    countryShow = "Unknown";
+                } else {
+                    // find the first origin returned in the last
+                    $(xml).find('usage_full').each(function (i) {
+                        if (i > 0) {
+                            return false;
+                        }
+                        // assign the returned origin to variable countryOrigin
+                        var countryOrigin = $(this).text();
+                        // console.log("origin", countryOrigin);
+
+                        // change variable to country name instead of origin, ie: from Spanish to Spain
+                        if (countryOrigin == "Catalan") {
+                            countryShow = "Catalonia";
+                        } else if (countryOrigin == "Chinese") {
+                            countryShow = "China";
+                        } else if (countryOrigin == "Czech") {
+                            countryShow = "Czech Republic";
+                        } else if (countryOrigin == "Danish") {
+                            countryShow = "Denmark";
+                        } else if (countryOrigin == "Dutch") {
+                            countryShow = "Netherlands";
+                        } else if (countryOrigin == "English") {
+                            countryShow = "England";
+                        } else if (countryOrigin == "Finnish") {
+                            countryShow = "Finland";
+                        } else if (countryOrigin == "French") {
+                            countryShow = "France";
+                        } else if (countryOrigin == "German") {
+                            countryShow = "Germany";
+                        } else if (countryOrigin == "Irish") {
+                            countryShow = "Ireland";
+                        } else if (countryOrigin == "Italian") {
+                            countryShow = "Italy";
+                        } else if (countryOrigin == "Japanese") {
+                            countryShow = "Japan";
+                        } else if (countryOrigin == "Norwegian") {
+                            countryShow = "Norway";
+                        } else if (countryOrigin == "Polish") {
+                            countryShow = "Poland";
+                        } else if (countryOrigin == "Portuguese") {
+                            countryShow = "Portugal";
+                        } else if (countryOrigin == "Russian") {
+                            countryShow = "Russia";
+                        } else if (countryOrigin == "Slovak") {
+                            countryShow = "Slovakia";
+                        } else if (countryOrigin == "Spanish") {
+                            countryShow = "Spain";
+                        } else if (countryOrigin == "Swedish") {
+                            countryShow = "Sweden";
+                        } else if (countryOrigin == "Ukrainian") {
+                            countryShow = "Ukraine";
+                        } else { // if not in this list, mark as unknown
+                            countryShow = "Unknown";
+                        }
+                    });
+                }
+                console.log(countryShow);
+                // show the country name in the dataShow div on index.html
+                $("#buttonsDiv").html('<h1>' + countryShow + '</h1>');
+                $("#buttonsDiv").append('<button class="btn maps" data-country="'+countryShow+'">MAP</button>');
+                $("#buttonsDiv").append('<button class="btn history" data-country="'+countryShow+'">HISTORY</button>');
+                $("#buttonsDiv").append('<button class="btn actors" data-name="'+searchTerm+'" data-country="'+countryShow+'">ACTORS</button>');
+            }
+        });
+    });
+});
