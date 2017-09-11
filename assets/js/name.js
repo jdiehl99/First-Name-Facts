0adb4ec413bb1f2cc9fc62e92b78fa387f5b5268
Jodi Diehl <29265610+jdiehl99@users.noreply.github.com>
2017-09-11 13:16:31 -0400

    added wikipedia page ID to countryPageID variable for each country

--- assets/js/name.js
+++ assets/js/name.js
@@ -1,6 +1,7 @@
 var countryOrigin = "";
 var countryShow = "";
 var searchTerm = "";
+var countryPageID = "";
 jQuery(($) => { // document on ready
 
     $(document).on("click", "#doSearch", function () {
@@ -39,46 +40,67 @@ jQuery(($) => { // document on ready
                         // change variable to country name instead of origin, ie: from Spanish to Spain
                         if (countryOrigin == "American") {
                             countryShow = "USA";
+                            countryPageID = "3434750";
                         } else if (countryOrigin == "Catalan") {
                             countryShow = "Catalonia";
+                            countryPageID = "6822";
                         } else if (countryOrigin == "Chinese") {
                             countryShow = "China";
+                            countryPageID = "5405";
                         } else if (countryOrigin == "Czech") {
                             countryShow = "Czech Republic";
+                            countryPageID = "5321";
                         } else if (countryOrigin == "Danish") {
                             countryShow = "Denmark";
+                            countryPageID = "76972";
                         } else if (countryOrigin == "Dutch") {
                             countryShow = "Netherlands";
+                            countryPageID = "21148";
                         } else if (countryOrigin == "English") {
-                            countryShow = "United_Kingdom";
+                            countryShow = "United Kingdom";
+                            countryPageID = "31717";
                         } else if (countryOrigin == "Finnish") {
                             countryShow = "Finland";
+                            countryPageID = "10577";
                         } else if (countryOrigin == "French") {
                             countryShow = "France";
+                            countryPageID = "5843419";
                         } else if (countryOrigin == "German") {
                             countryShow = "Germany";
+                            countryPageID = "11867";
                         } else if (countryOrigin == "Irish") {
                             countryShow = "Ireland";
+                            countryPageID = "147575";
                         } else if (countryOrigin == "Italian") {
                             countryShow = "Italy";
+                            countryPageID = "14532";
                         } else if (countryOrigin == "Japanese") {
                             countryShow = "Japan";
+                            countryPageID = "15573";
                         } else if (countryOrigin == "Norwegian") {
                             countryShow = "Norway";
+                            countryPageID = "21241";
                         } else if (countryOrigin == "Polish") {
                             countryShow = "Poland";
+                            countryPageID = "22936";
                         } else if (countryOrigin == "Portuguese") {
                             countryShow = "Portugal";
+                            countryPageID = "23033";
                         } else if (countryOrigin == "Russian") {
                             countryShow = "Russia";
+                            countryPageID = "25391";
                         } else if (countryOrigin == "Slovak") {
                             countryShow = "Slovakia";
+                            countryPageID = "26830";
                         } else if (countryOrigin == "Spanish") {
                             countryShow = "Spain";
+                            countryPageID = "26667";
                         } else if (countryOrigin == "Swedish") {
                             countryShow = "Sweden";
+                            countryPageID = "5058739";
                         } else if (countryOrigin == "Ukrainian") {
                             countryShow = "Ukraine";
+                            countryPageID = "31750";
                         } else { // if not in this list, mark as unknown
                             countryShow = "Unknown";
                         }
@@ -88,7 +110,7 @@ jQuery(($) => { // document on ready
 
                 $("#dataShow").append('<h1>' + countryShow + '</h1>');
                 $("#dataShow").append('<button class="btn maps" data-country="'+countryShow+'">MAP</button>');
-                $("#dataShow").append('<button class="btn history" data-country="'+countryShow+'">HISTORY</button>');
+                $("#dataShow").append('<button class="btn history" data-pageID="'+countryPageID+'" data-country="'+countryShow+'">HISTORY</button>');
                 $("#dataShow").append('<button class="btn actors" data-name="'+searchTerm+'" data-country="'+countryShow+'">ACTORS</button>');
 
             }
