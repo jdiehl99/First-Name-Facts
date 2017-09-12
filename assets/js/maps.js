6a1e84e976dd93f31f87b85bacef1662afa23035
Jodi Diehl <29265610+jdiehl99@users.noreply.github.com>
2017-09-12 16:50:29 -0400

    added js call to index.html for google maps

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -4,6 +4,22 @@ $(document).on("click", ".maps", function () {
 
     var countryShow = $(this).attr("data-country");
     var flagInfo = $(this).attr("data-flag");
+
+    // google maps API key AIzaSyDrHgJVlCVFzRNb_R4wjMg38LyZOxsIF4k
+    // 
+    //
+
+    var mapQuery = "https://maps.googleapis.com/maps/api/geocode/json?address="+countryShow+"""&key=AIzaSyDrHgJVlCVFzRNb_R4wjMg38LyZOxsIF4k";
     
+
+    //   var map;
+    //   function initMap() {
+    //     map = new google.maps.Map(document.getElementById('map'), {
+    //       center: {lat: -34.397, lng: 150.644},
+    //       zoom: 8
+    //     });
+    //   }
+
+
     $("#dataShow").append('<p>Maps page - ' + countryShow + ' was passed through from button on Name Page</p>');
 });
