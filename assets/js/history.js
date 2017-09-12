0320e2c0f560ef0ba4a98f16d9cf70934a9b2bca
mariogmazza <mariomazza01@gmail.com>
2017-09-12 15:46:44 -0400

    first wikiapi call no pageID

--- assets/js/history.js
+++ assets/js/history.js
@@ -12,14 +12,43 @@ $(document).ready(function(){
 
 
     var countryHistory;
+
     if(countryShow!=="Unknown"){
-    countryHistory=factbook.countries[countryShow.toLowerCase()].data.introduction.background
+    // countryHistory=factbook.countries[countryShow.toLowerCase()].data.introduction.background
+
+
+    https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro=true&format=json&titles=norway
+
+    var JSONSite = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=true&titles=";
+    JSONSite += countryShow;
+    JSONSite += '&callback=?';
+
+    // alert("This is executed normally.");
+
+    $.getJSON(JSONSite,  function (data)
+    {
+        console.log(data.query.pages["extract"]);
+    });
+// var urlApi="https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=Stack%20Overflow";
+
+        // $.ajax({
+        //     type: 'GET',   
+        //     dataType: "json",
+        //     // url: urlApi,
+        //     url:urlApi,
+        //     // method:"GET"
+        // }).done(function(data){
+        //     console.log("you are cool");
+        //     console.log(data);
+
+        // })
+
+
     }else{
       countryHistory="Unknown Origin";
       }
 
     $("#dataShow").append('<p>'+countryHistory+'</p>');
-
 
 
 });
