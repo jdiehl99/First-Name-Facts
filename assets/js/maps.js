234af50bf5eb10c0c728ad6e1cc93544e1b7827b
Jodi Diehl <29265610+jdiehl99@users.noreply.github.com>
2017-09-15 13:17:21 -0400

    testing if map will load

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,25 +1,37 @@
-$(document).on("click", ".maps", function () {
-    console.log("maps button was clicked");
-    event.preventDefault();
+$(document).ready(function () {
+    $(document).on("click", ".maps", function () {
+        console.log("maps button was clicked");
+        event.preventDefault();
 
-    var countryShow = $(this).attr("data-country");
-    var flagInfo = $(this).attr("data-flag");
+        var countryShow = $(this).attr("data-country");
+        var flagInfo = $(this).attr("data-flag");
 
-    // google maps API key AIzaSyDrHgJVlCVFzRNb_R4wjMg38LyZOxsIF4k
-    // 
-    //
-
-    var mapQuery = "https://maps.googleapis.com/maps/api/geocode/json?address="+countryShow+"""&key=AIzaSyDrHgJVlCVFzRNb_R4wjMg38LyZOxsIF4k";
-    
-
-    //   var map;
-    //   function initMap() {
-    //     map = new google.maps.Map(document.getElementById('map'), {
-    //       center: {lat: -34.397, lng: 150.644},
-    //       zoom: 8
-    //     });
-    //   }
+        // google maps API key AIzaSyDrHgJVlCVFzRNb_R4wjMg38LyZOxsIF4k
 
 
-    $("#dataShow").append('<p>Maps page - ' + countryShow + ' was passed through from button on Name Page</p>');
+        var mapQuery = "https://maps.googleapis.com/maps/api/geocode/json?address=" + countryShow + "&key=AIzaSyDrHgJVlCVFzRNb_R4wjMg38LyZOxsIF4k";
+        console.log(mapQuery);
+        $.ajax({
+            url: mapQuery,
+            method: 'GET'
+        }).done(function (results) {
+            console.log(results);
+            // grab latitude and longitude from results and put into variables
+
+        });
+
+        $("#dataShow").append('<p>Maps page - ' + countryShow + ' was passed through from button on Name Page</p>');
+
+        // pass through Longitude and Latitude to create map
+        function myMap() {
+            var mapProp = {
+                center: new google.maps.LatLng(51.508742, -0.120850),
+                zoom: 5,
+            };
+            var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+        }
+        // why won't this recognize myMap function?  It works fine outside of the on.ready
+        $.getScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyDrHgJVlCVFzRNb_R4wjMg38LyZOxsIF4k&callback=myMap");
+
+    });
 });
