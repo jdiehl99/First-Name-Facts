87a007b2d04dbcf69f2685f41dfecc91061225e0
mariogmazza <mariomazza01@gmail.com>
2017-09-16 13:09:01 -0400

    working wiki call and display to dom

--- assets/js/history.js
+++ assets/js/history.js
@@ -1,61 +1,47 @@
-
-$(document).ready(function(){
-    
+$(document).ready(function () {
 
 
-   $(document).on("click", ".history", function () {
 
-    console.log("history button was clicked");
-    event.preventDefault();
+    $(document).on("click", ".history", function () {
 
-    var countryShow = $(this).attr("data-country");
-    var countryID = $(this).attr("data-pageID");
-    var flagInfo = $(this).attr("data-flag");
+        console.log("history button was clicked");
+        event.preventDefault();
+
+        var countryShow = $(this).attr("data-country");
+        var countryID = $(this).attr("data-pageID");
+        var flagInfo = $(this).attr("data-flag");
+        var countryHistory;
+
+        if (countryShow !== "Unknown") {
+
+            var JSONSite = "https://en.wikipedia.org/w/api.php?action=query&formatversion=2&exintro=true&prop=extracts&format=json&pageids=";
+            JSONSite += countryID;
+            JSONSite += '&callback=?';
+
+            $.ajax({
+                url: JSONSite,
+                dataType: 'jsonp',
+                method: "GET"
+            }).done(function (data) {
+
+                // console.log(data.query.pages["0"].extract);
+                countryHistory = data.query.pages["0"];
+                console.log(countryHistory.extract);
+                $("#HistoryOutput").empty();
+                $("#HistoryOutput").append(countryHistory.extract);
+            })
 
 
-    var countryHistory;
+        } else {
+            countryHistory = "Unknown Origin"
+            $("#HistoryOutput").empty();
+            $("#HistoryOutput").append(countryHistory);
 
-    if(countryShow!=="Unknown"){
-    // countryHistory=factbook.countries[countryShow.toLowerCase()].data.introduction.background
+        }
 
 
-    https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro=true&format=json&titles=norway
-
-    var JSONSite = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=true&titles=";
-    JSONSite += countryShow;
-    JSONSite += '&callback=?';
-
-    // alert("This is executed normally.");
-
-    $.getJSON(JSONSite,  function (data)
-    {
-        console.log(data.query.pages["extract"]);
     });
-// var urlApi="https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=Stack%20Overflow";
 
-        // $.ajax({
-        //     type: 'GET',   
-        //     dataType: "json",
-        //     // url: urlApi,
-        //     url:urlApi,
-        //     // method:"GET"
-        // }).done(function(data){
-        //     console.log("you are cool");
-        //     console.log(data);
-
-        // })
-
-
-    }else{
-      countryHistory="Unknown Origin";
-      }
-
-    $("#dataShow").append('<p>'+countryHistory+'</p>');
 
 
 });
-
-
-    
-  });//end of $(document)
-
