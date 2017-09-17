ff74256abd81dcd06533ff177b1025ae0809e502
Jodi Diehl <29265610+jdiehl99@users.noreply.github.com>
2017-09-17 15:22:06 -0400

    created stand along html page for map to display country

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,37 +1,40 @@
+function initMap() {
+    map = new google.maps.Map(document.getElementById('theMap'), {
+        center: { lat: latInfo, lng: longInfo },
+        zoom: 4
+    });
+    var marker = new google.maps.Marker({
+        position: { lat: latInfo, lng: longInfo },
+        map: map
+    });
+}
+
 $(document).ready(function () {
     $(document).on("click", ".maps", function () {
         console.log("maps button was clicked");
         event.preventDefault();
 
         var countryShow = $(this).attr("data-country");
-        var flagInfo = $(this).attr("data-flag");
 
         // google maps API key AIzaSyDrHgJVlCVFzRNb_R4wjMg38LyZOxsIF4k
-
+        var latInfo;
+        var longInfo;
+        var map;
 
         var mapQuery = "https://maps.googleapis.com/maps/api/geocode/json?address=" + countryShow + "&key=AIzaSyDrHgJVlCVFzRNb_R4wjMg38LyZOxsIF4k";
-        console.log(mapQuery);
+        console.log("country", countryShow);
+        console.log("query", mapQuery);
         $.ajax({
             url: mapQuery,
             method: 'GET'
-        }).done(function (results) {
-            console.log(results);
+        }).done(function (response) {
+            console.log(response);
             // grab latitude and longitude from results and put into variables
-
+            latInfo = response.results["0"].geometry.location.lat;
+            longInfo = response.results["0"].geometry.location.lng;
+            console.log("lat", latInfo);
+            console.log("long", longInfo);
+            $.getScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyDrHgJVlCVFzRNb_R4wjMg38LyZOxsIF4k&callback=initMap");
         });
-
-        $("#dataShow").append('<p>Maps page - ' + countryShow + ' was passed through from button on Name Page</p>');
-
-        // pass through Longitude and Latitude to create map
-        function myMap() {
-            var mapProp = {
-                center: new google.maps.LatLng(51.508742, -0.120850),
-                zoom: 5,
-            };
-            var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-        }
-        // why won't this recognize myMap function?  It works fine outside of the on.ready
-        $.getScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyDrHgJVlCVFzRNb_R4wjMg38LyZOxsIF4k&callback=myMap");
-
     });
 });
