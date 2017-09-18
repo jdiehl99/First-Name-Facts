6003695ccdee73bfaa4e8b185ba2dffb6a0740f3
Ciarra Murphy <mciarra@ymail.com>
2017-09-18 11:41:44 -0400

    added basic ajax outline for name search

--- assets/js/history.js
+++ assets/js/history.js
@@ -33,7 +33,7 @@ $(document).ready(function () {
             $("#HistoryOutput").append(countryHistory);
 
         }
-
+        
 
     });
 });
---
+++ assets/js/popularity.js
@@ -0,0 +1,10 @@
+
+var nameSearch
+var namePop = "https://api.fullcontact.com/v2/name/stats.json?name=michael&apiKey=e957faa606a34537";
+
+$.ajax({
+    url: namePop,
+    method: 'GET',
+}).done(function(reponse){
+    console.log(response);
+})
