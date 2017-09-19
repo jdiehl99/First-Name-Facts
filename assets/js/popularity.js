5e2d175781588f554a3623eb2bc64fcbd605e8d1
Jodi Diehl <29265610+jdiehl99@users.noreply.github.com>
2017-09-19 12:07:28 -0400

    udpated css

--- assets/js/popularity.js
+++ assets/js/popularity.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     //when you click the submit button you get the results
     $(document).on("click", "#doSearch", function () {
-        var nameSearch = $("#searchInput").val().trim();
+        var nameSearch = $(".searchInput").val().trim();
         var namePop = "https://api.fullcontact.com/v2/name/stats.json?name=" + nameSearch + "&apiKey=e957faa606a34537";
 
         $.ajax({
