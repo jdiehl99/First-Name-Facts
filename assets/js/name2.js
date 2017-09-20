a07e1a725a5a0cd92c67f01e13f3a1413ec379fc
Jodi Diehl <29265610+jdiehl99@users.noreply.github.com>
2017-09-20 12:09:41 -0400

    created name2.js to handle interior searches

--- assets/js/name2.js
+++ assets/js/name2.js
@@ -1,6 +1,6 @@
 var countryOrigin = "";
 var countryShow = "";
-var searchTerm = "";
+var searchTerm2 = "";
 var countryPageID = "";
 var flagImg = "";
 
@@ -15,11 +15,14 @@ $(".errorMg").hide()
 
 jQuery(($) => { // document on ready
 
-    $(document).on("click", "#doSearch", function () {
+    $(document).on("click", "#doSearch2", function () {
 
 
-        console.log("search button was clicked");
+        console.log("search button 2 was clicked");
         event.preventDefault();
+        // grab the name from the input box
+        var searchTerm2 = $(".searchInput2").val().trim();
+        console.log("search term 2", searchTerm2);
 
         // show buttons div 
         $("#buttonsDiv").show();
@@ -36,13 +39,9 @@ jQuery(($) => { // document on ready
         // hide title header
         $(".hero").hide();
 
-        // grab the name from the input box
-        var searchTerm = $(".searchInput").val().trim();
-        console.log("search term",searchTerm);
-
         //validates the user input to make sure is either a non-empty string or
         // that there arent any numbers or spaces
-        if (validate(searchTerm)) {
+        if (validate(searchTerm2)) {
 
             //show graph if search is good
             $("#ngramData").show();
@@ -51,7 +50,7 @@ jQuery(($) => { // document on ready
             // send the request to BehindTheName using ajax
             $.ajax({
                 type: "GET",
-                url: "https://www.behindthename.com/api/lookup.php?name=" + searchTerm + "&key=jo493898",
+                url: "https://www.behindthename.com/api/lookup.php?name=" + searchTerm2 + "&key=jo493898",
                 dataType: "xml",
                 success: function (xml) {
 
@@ -168,13 +167,13 @@ jQuery(($) => { // document on ready
                         longInfo = response.results["0"].geometry.location.lng;
 
                         // show the country name in the dataShow div on index.html
-                        $("#dataShow").html('<h1 class="countryOrig">You searched for: '+searchTerm+'</h1><h1 class="countryOrig">Your name originates from: ' + countryShow + '</h1>');
-
+                        $("#dataShow").html('<h1 class="countryOrig">You searched for: '+searchTerm2+'</h1><h1 class="countryOrig">Your name originates from: ' + countryShow + '</h1>');
+                        
                         // create links for navigation between APIs
-                        $("#buttonsDiv").append('<a href="#" id="doSearch" class="btn name" searchInput="' + searchTerm + '" data-lat="' + latInfo + '"data-long="' + longInfo + '" data-flag="' + flagImg + '" data-country="' + countryShow + '">STATS</a>');
-                        $("#buttonsDiv").append('<a href="#" class="btn maps" searchInput="' + searchTerm + '" data-lat="' + latInfo + '"data-long="' + longInfo + '" data-flag="' + flagImg + '" data-country="' + countryShow + '">MAP</a>');
-                        $("#buttonsDiv").append('<a href="#" class="btn history" searchInput="' + searchTerm + '" data-flag="' + flagImg + '" data-pageID="' + countryPageID + '" data-country="' + countryShow + '">HISTORY</a>');
-                        $("#buttonsDiv").append('<a href="#" class="btn actors" searchInput="' + searchTerm + '" data-name="' + searchTerm + '" data-country="' + countryShow + '">ACTORS</a>');
+                        $("#buttonsDiv").append('<a href="#" id="doSearch" class="btn name" searchInput="' + searchTerm2 + '" data-lat="' + latInfo + '"data-long="' + longInfo + '" data-flag="' + flagImg + '" data-country="' + countryShow + '">STATS</a>');
+                        $("#buttonsDiv").append('<a href="#" class="btn maps" searchInput="' + searchTerm2 + '" data-lat="' + latInfo + '"data-long="' + longInfo + '" data-flag="' + flagImg + '" data-country="' + countryShow + '">MAP</a>');
+                        $("#buttonsDiv").append('<a href="#" class="btn history" searchInput="' + searchTerm2 + '" data-flag="' + flagImg + '" data-pageID="' + countryPageID + '" data-country="' + countryShow + '">HISTORY</a>');
+                        $("#buttonsDiv").append('<a href="#" class="btn actors" searchInput="' + searchTerm2 + '" data-name="' + searchTerm2 + '" data-country="' + countryShow + '">ACTORS</a>');
 
                         //move the search bar to the navbarbar
                         $("#buttonsDiv").append('<div class="field has-addons"> <div id="control"> <input class="input is-primary is-outlined searchInput2" type="text"></div><div id="control"><input class="button is-primary" type="submit" id="doSearch2" value="Submit"></div></div>')
@@ -187,7 +186,7 @@ jQuery(($) => { // document on ready
 
         } //end of search term validator
 
-        $('#ngramData').html('<iframe name="ngram_chart" src="https://books.google.com/ngrams/interactive_chart?content=' + searchTerm + '&year_start=1800&year_end=2008&corpus=0&smoothing=3&share=&direct_url=t1%3B%2C' + searchTerm + '%3B%2Cc0" width="100%" height="500px" marginwidth=0 marginheight=0 hspace=0 vspace=0 frameborder=0 scrolling=no></iframe>');
+        $('#ngramData').html('<iframe name="ngram_chart" src="https://books.google.com/ngrams/interactive_chart?content=' + searchTerm2 + '&year_start=1800&year_end=2008&corpus=0&smoothing=3&share=&direct_url=t1%3B%2C' + searchTerm2 + '%3B%2Cc0" width="100%" height="500px" marginwidth=0 marginheight=0 hspace=0 vspace=0 frameborder=0 scrolling=no></iframe>');
 
     });
 
