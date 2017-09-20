8965f324937ac1cca92f7de0384fe41cb0c4bb68
Ciarra Murphy <mciarra@ymail.com>
2017-09-20 13:28:56 -0400

    added pseudocode to history and popularity pages

--- assets/js/popularity.js
+++ assets/js/popularity.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
-    //when you click the submit button you get the results
+    //get the name entered when the search button is clicked
     $(document).on("click", "#doSearch", function () {
         //grabs the information for the popularity of the searchTerm
         var nameSearch = $(".searchInput").val().trim();
         var namePop = "https://api.fullcontact.com/v2/name/stats.json?name=" + nameSearch + "&apiKey=e957faa606a34537";
-
+        //sends name request through ajax
         $.ajax({
         url: namePop,
         method: 'GET',
+        //displays the information related to the name
         }).done(function(response){
         //puts the number of men and women with the name on the page as well as their average age
         $("#popData").append("women:",response.name.given.female.count);
