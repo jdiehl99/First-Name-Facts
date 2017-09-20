7e4492e2c03124d357c66f9b735519d2f9894571
Ciarra Murphy <mciarra@ymail.com>
2017-09-20 13:14:11 -0400

    added first pseudocode

--- assets/js/popularity.js
+++ assets/js/popularity.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     //when you click the submit button you get the results
     $(document).on("click", "#doSearch", function () {
+        //grabs the information for the popularity of the searchTerm
         var nameSearch = $(".searchInput").val().trim();
         var namePop = "https://api.fullcontact.com/v2/name/stats.json?name=" + nameSearch + "&apiKey=e957faa606a34537";
 
@@ -8,8 +9,8 @@ $(document).ready(function(){
         url: namePop,
         method: 'GET',
         }).done(function(response){
-        //added the parameters of the name
-        $("#popData").html("women:",response.name.given.female.count);
+        //puts the number of men and women with the name on the page as well as their average age
+        $("#popData").append("women:",response.name.given.female.count);
         $("#popData").append("men:",response.name.given.male.count);
         $("#popData").append("average age for women:", response.name.given.female.age.densityCurve.meanAge);
         $("#popData").append("average age for men:", response.name.given.male.age.densityCurve.meanAge);
