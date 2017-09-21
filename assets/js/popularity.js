521be7c431f9e8b0bdb63572d9144477242a4e0d
Jodi Diehl <29265610+jdiehl99@users.noreply.github.com>
2017-09-20 20:58:54 -0400

    Update popularity.js

--- assets/js/popularity.js
+++ assets/js/popularity.js
@@ -12,8 +12,9 @@ $(document).ready(function(){
         //displays the information related to the name
         }).done(function(response){
 
-        //added the parameters of the name
-        var datBox = $('#popData'),
+            $("#popData").html('<p class="is-size-7">The table below shows the number of people in the US that have the same name by gender as well as the average age for each group.</p>');
+        //create table of data from census
+        var datBox = $('#popData');
         table = $('<table class="poppD">');
         var tr = $('<tr><td class="poppD">Women:</td>');
         tr.append('<td class="poppD">' + response.name.given.female.count + '</td>');
